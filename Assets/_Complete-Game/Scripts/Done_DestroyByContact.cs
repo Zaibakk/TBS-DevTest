be36318b262b4cc3e9b1bf08d868628563243c55
pc-federico\Federico <f.distefano87@hotmail.it>
2019-02-07 22:59:58 +0100

    fatti super nemici

--- Assets/_Complete-Game/Scripts/Done_DestroyByContact.cs
+++ Assets/_Complete-Game/Scripts/Done_DestroyByContact.cs
@@ -5,7 +5,10 @@ public class Done_DestroyByContact : MonoBehaviour
 {
 	public GameObject explosion;
 	public int scoreValue;
+    public bool doubleLife;
 	private Done_GameController gameController;
+    private Color original;
+
 
 	void Start ()
 	{
@@ -18,7 +21,9 @@ public class Done_DestroyByContact : MonoBehaviour
 		{
 			Debug.Log ("Cannot find 'GameController' script");
 		}
-	}
+        if (doubleLife)
+            setRed();
+    }
 
 	void OnTriggerEnter (Collider other)
 	{
@@ -26,18 +31,39 @@ public class Done_DestroyByContact : MonoBehaviour
 		{
 			return;
 		}
-
-		if (explosion != null)
-		{
-			Instantiate(explosion, transform.position, transform.rotation);
-		}
-
-		if (other.tag == "Player")
-		{
-            other.gameObject.GetComponent<Done_PlayerController>().loseLife();
-		}
-		
-		gameController.AddScore(scoreValue);
-		Destroy (gameObject);
+        if (other.tag != "Player")
+            Destroy(other.gameObject);
+        if (!doubleLife)
+        {
+            if (explosion != null)
+            {
+                Instantiate(explosion, transform.position, transform.rotation);
+            }
+            if (other.tag == "Player")
+            {
+                other.gameObject.GetComponent<Done_PlayerController>().loseLife();
+            }
+            gameController.AddScore(scoreValue);
+            Destroy(gameObject);
+        }
+        else
+        {
+            doubleLife = false;
+            setStandard(); 
+        }
 	}
+    
+    void setRed()
+    {
+        Renderer rend = gameObject.transform.GetChild(2).GetComponent<Renderer>();
+        original = rend.material.GetColor("_Color");
+        rend.material.SetColor("_Color", new Color(1.0f,0.0f,0.0f));
+    }
+
+    void setStandard()
+    {
+        Renderer rend = gameObject.transform.GetChild(2).GetComponent<Renderer>();
+        rend.material.SetColor("_Color", original);
+    }
+
 }
